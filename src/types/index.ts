549ddb6756086d553fc9c4ce1498e951d4fbb55d
wuxiaodong <wuxiaodong@zhuku.co>
2019-10-08 12:28:00 +0800

    添加cancel token方法 添加axios工厂方法

--- src/types/index.ts
+++ src/types/index.ts
@@ -16,11 +16,12 @@ export interface AxiosRequestConfig {
   timeout?: number
   transformRequest?: AxiosTransformer | AxiosTransformer[]
   transformResponse?: AxiosTransformer | AxiosTransformer[]
+  cancelToken?: CancelToken
   
   [propName: string]: any
 }
 
-export interface AxiosResponse<T = any > {
+export interface AxiosResponse<T = any> {
   data: T
   status: number
   statusText: string
@@ -29,11 +30,11 @@ export interface AxiosResponse<T = any > {
   request: any
 }
 
-export interface AxiosPromise<T = any> extends Promise<AxiosResponse<T>>{
+export interface AxiosPromise<T = any> extends Promise<AxiosResponse<T>> {
 
 }
 
-export interface AxiosError extends Error{
+export interface AxiosError extends Error {
   isAxiosError: boolean
   config: AxiosRequestConfig
   code?: string | null
@@ -48,37 +49,72 @@ export interface Axios {
     response: AxiosInterceptorManager<AxiosResponse>
   }
   
-  request<T = any >(config:AxiosRequestConfig): AxiosPromise<T>
+  request<T = any>(config: AxiosRequestConfig): AxiosPromise<T>
   
-  get<T = any >(url:string,config?: AxiosRequestConfig): AxiosPromise<T>
-  delete<T = any >(url:string,config?: AxiosRequestConfig): AxiosPromise<T>
-  head<T = any >(url:string,config?: AxiosRequestConfig): AxiosPromise<T>
-  options<T = any >(url:string,config?: AxiosRequestConfig): AxiosPromise<T>
+  get<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
   
-  post<T = any >(url:string,data?: any, config?: AxiosRequestConfig): AxiosPromise<T>
-  put<T = any >(url:string,data?: any, config?: AxiosRequestConfig): AxiosPromise<T>
-  patch<T = any >(url:string,data?: any, config?: AxiosRequestConfig): AxiosPromise<T>
+  delete<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
+  
+  head<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
+  
+  options<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
+  
+  post<T = any>(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise<T>
+  
+  put<T = any>(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise<T>
+  
+  patch<T = any>(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise<T>
 }
 
-export interface AxiosInstance extends  Axios{
+export interface AxiosStatic extends AxiosInstance {
+  create(config: AxiosRequestConfig): AxiosInstance
+}
+
+export interface AxiosInstance extends Axios {
   <T>(config: AxiosRequestConfig): AxiosPromise<T>
-  <T>(url:string,config?:AxiosRequestConfig): AxiosPromise<T>
+  
+  <T>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
 }
 
 export interface AxiosInterceptorManager<T> {
-  use(resolved:ResolvedFn<T>, rejected?: RejectedFn) : number
+  use(resolved: ResolvedFn<T>, rejected?: RejectedFn): number
   
-  eject(id: number) : void
+  eject(id: number): void
 }
 
 export interface ResolvedFn<T> {
-  (val:T):T|Promise<T>
+  (val: T): T | Promise<T>
 }
 
 export interface RejectedFn {
-  (error: any) : any
+  (error: any): any
 }
 
 export interface AxiosTransformer {
-  (data:any, headers?: any): any
+  (data: any, headers?: any): any
+}
+
+export interface CancelToken {
+  promise: Promise<string>
+  reason?: string
+}
+
+export interface Canceler {
+  (message?: string): void
+}
+
+export interface cancelExecutor {
+  (cancel: Canceler): void
+}
+
+export interface CancelTokenSource {
+  token:CancelToken
+  cancel:Canceler
+}
+
+export interface CancelTokenStatic {
+  new(executor:cancelExecutor): CancelToken
+  
+  source(): CancelTokenSource
+  
 }
