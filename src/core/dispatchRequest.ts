859337e951bd6a95ef66b49e161b17e1003cf4cf
wuxiaodong <wuxiaodong@zhuku.co>
2019-10-08 11:50:11 +0800

    扩展transform 方法

--- src/core/dispatchRequest.ts
+++ src/core/dispatchRequest.ts
@@ -1,8 +1,8 @@
 import { AxiosPromise, AxiosRequestConfig, AxiosResponse } from '../types'
 import xhr from './xhr'
 import { buildUrl } from '../helpers/url'
-import { transformRequest, transformResponse } from '../helpers/data'
-import { flattenHeaders, processHeaders } from '../helpers/headers'
+import { flattenHeaders } from '../helpers/headers'
+import transform from './transform'
 
 export default function axios(config: AxiosRequestConfig): AxiosPromise {
   // TODO
@@ -14,8 +14,7 @@ export default function axios(config: AxiosRequestConfig): AxiosPromise {
 
 function processConfig(config: AxiosRequestConfig): void {
   config.url = transformURL(config)
-  config.headers = transformHeaders(config)
-  config.data = transformRequestData(config)
+  config.data = transform(config.data, config.headers, config.transformRequest)
   config.headers = flattenHeaders(config.headers, config.method!)
 }
 
@@ -24,16 +23,7 @@ function transformURL(config: AxiosRequestConfig): string {
   return buildUrl(url!, params)
 }
 
-function transformRequestData(config: AxiosRequestConfig): any {
-  return transformRequest(config.data)
-}
-
-function transformHeaders(config: AxiosRequestConfig): any {
-  const { data, headers = {} } = config
-  return processHeaders(headers, data)
-}
-
 function transformResponseData(res: AxiosResponse): AxiosResponse {
-  res.data = transformResponse(res.data)
+  res.data = transform(res.data, res.headers, res.config.transformResponse)
   return res
 }
